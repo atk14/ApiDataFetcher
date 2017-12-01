e292ee38dcab63ad1c3996d5f8b4e2425fb01f31
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-12-01 21:35:39 +0100

    Test fixed

--- test/tc_api_data_fetcher.php
+++ test/tc_api_data_fetcher.php
@@ -2,19 +2,40 @@
 class TcApiDataFetcher extends TcBase {
 
 	function test(){
-		$apf = new ApiDataFetcher("http://skelet.atk14.net/api/",array(
+		$apf = new ApiDataFetcher("http://www.atk14.net/api/",array(
 			"logger" => new Logger()
 		));
 
-		// ### login_availabilities/
+		$login = "testing".uniqid();
+
+		// ### login_availabilities
 
 		$data = $apf->post("login_availabilities/detail",array(
-			"login" => "badass",
+			"login" => $login,
 		));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals(array("status" => "available"),$data);
 
-		// ### login/create_new
+		// ### users/create_new
+
+		$data = $apf->post("users/create_new",array(
+			"login" => $login,
+			"name" => "John Doe",
+			"email" => "john@doe.com",
+			"password" => "secret1234",
+		));
+
+		$this->assertEquals(201,$apf->getStatusCode());
+
+		// ### login_availabilities
+
+		$data = $apf->post("login_availabilities/detail",array(
+			"login" => $login,
+		));
+		$this->assertEquals(200,$apf->getStatusCode());
+		$this->assertEquals(array("status" => "taken"),$data);
+
+		// ### logins/create_new
 
 		$options = array(
 			"acceptable_error_codes" => array(
@@ -31,18 +52,18 @@ class TcApiDataFetcher extends TcBase {
 		$this->assertNull($data);
 
 		$data = $apf->post("logins/create_new",array(
-			"login" => "admin",
+			"login" => $login,
 			"password" => "badTRY"
 		),$options);
 		$this->assertEquals(401,$apf->getStatusCode());
 		$this->assertNull($data);
 
 		$data = $apf->post("logins/create_new",array(
-			"login" => "admin",
-			"password" => "admin"
+			"login" => $login,
+			"password" => "secret1234"
 		),$options);
 		$this->assertEquals(201,$apf->getStatusCode());
 		$this->assertInternalType("array",$data);
-		$this->assertEquals("admin",$data["login"]);
+		$this->assertEquals($login,$data["login"]);
 	}
 }
