7d5be3526fd475f8e790255245212d88fc7760cd
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-12-01 17:51:36 +0100

    File upload testing rewritten

--- test/tc_file_upload.php
+++ test/tc_file_upload.php
@@ -2,34 +2,17 @@
 class TcFileUpload extends TcBase {
 
 	function test(){
-		$adf = new ApiDataFetcher("http://pupiq_srv.localhost/api/",array(
+		// There is a testing API method for file upload on ATK14 website.
+		// See http://www.atk14.net/api/en/file_uploads/create_new/
+
+		$adf = new ApiDataFetcher("http://www.atk14.net/api/",array(
 			"logger" => new Logger()
 		));
-		$data = $adf->postFile("attachments/create_new","sandokan.jpg",array(
-			"auth_token" => $this->_getAuthToken()
-		));
+		$data = $adf->postFile("file_uploads/create_new","sandokan.jpg");
 
 		$this->assertEquals(201,$adf->getStatusCode());
 		$this->assertEquals("sandokan.jpg",$data["filename"]);
-
-		$adf = new ApiDataFetcher("http://pupiq_srv.localhost/api/");
-		$data = $adf->postFile("attachments/create_new",array(
-			"path" => __DIR__ . "/sandokan.jpg",
-			"name" => "česýlko.jpg",
-		),
-		array(
-			"auth_token" => $this->_getAuthToken(),
-		));
-
-		$this->assertEquals(201,$adf->getStatusCode());
-		$this->assertEquals("cesylko.jpg",$data["filename"]);
-	}
-
-	function _getAuthToken(){
-		$API_KEY = "1.k8qVPFrubmdosxhU7jRSc3Qp2NHW5gJTMviGDlzK";
-		$USER_ID = 1;
-		$current_time = time();
-		$t = $current_time - ($current_time % (60 * 10)); // kazdych 10 minut jiny token
-		return $USER_ID.".".hash("sha256",$API_KEY.$t);
+		$this->assertEquals("image/jpeg",$data["mime_type"]);
+		$this->assertEquals("d5d4599e3586064e0524d18e8ee8bce5",$data["md5sum"]);
 	}
 }
