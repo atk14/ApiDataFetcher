42445606bc4b6de2112ae271bbe5781b60248c11
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-02-16 01:08:03 +0100

    Testing

--- test/tc_api_data_fetcher.php
+++ test/tc_api_data_fetcher.php
@@ -2,7 +2,7 @@
 class TcApiDataFetcher extends TcBase {
 
 	function test(){
-		$apf = new ApiDataFetcher("http://www.atk14.net/api/",array(
+		$adf = new ApiDataFetcher("http://www.atk14.net/api/",array(
 			"logger" => new Logger()
 		));
 
@@ -10,33 +10,33 @@ class TcApiDataFetcher extends TcBase {
 
 		// ### login_availabilities
 
-		$data = $apf->get("login_availabilities/detail",array(
+		$data = $adf->get("login_availabilities/detail",array(
 			"login" => $login,
 		));
-		$this->assertEquals(200,$apf->getStatusCode());
+		$this->assertEquals(200,$adf->getStatusCode());
 		$this->assertEquals(array("status" => "available"),$data);
-		$this->assertEquals("GET",$apf->getLastMethod());
-		$this->assertEquals("http://www.atk14.net/api/en/login_availabilities/detail/?login=$login&format=json",$apf->getLastUrl());
+		$this->assertEquals("GET",$adf->getLastMethod());
+		$this->assertEquals("http://www.atk14.net/api/en/login_availabilities/detail/?login=$login&format=json",$adf->getLastUrl());
 
 		// ### users/create_new
 
-		$data = $apf->post("users/create_new",array(
+		$data = $adf->post("users/create_new",array(
 			"login" => $login,
 			"name" => "John Doe",
 			"email" => "john@doe.com",
 			"password" => "secret1234",
 		));
 
-		$this->assertEquals(201,$apf->getStatusCode());
-		$this->assertEquals("POST",$apf->getLastMethod());
-		$this->assertEquals("http://www.atk14.net/api/en/users/create_new/",$apf->getLastUrl());
+		$this->assertEquals(201,$adf->getStatusCode());
+		$this->assertEquals("POST",$adf->getLastMethod());
+		$this->assertEquals("http://www.atk14.net/api/en/users/create_new/",$adf->getLastUrl());
 
 		// ### login_availabilities
 
-		$data = $apf->get("login_availabilities/detail",array(
+		$data = $adf->get("login_availabilities/detail",array(
 			"login" => $login,
 		));
-		$this->assertEquals(200,$apf->getStatusCode());
+		$this->assertEquals(200,$adf->getStatusCode());
 		$this->assertEquals(array("status" => "taken"),$data);
 
 		// ### logins/create_new
@@ -48,67 +48,67 @@ class TcApiDataFetcher extends TcBase {
 			)
 		);
 		
-		$data = $apf->post("logins/create_new",array(
+		$data = $adf->post("logins/create_new",array(
 			"login" => "badass",
 			"password" => "badTRY"
 		),$options);
-		$this->assertEquals(404,$apf->getStatusCode());
+		$this->assertEquals(404,$adf->getStatusCode());
 		$this->assertNull($data);
 
-		$data = $apf->post("logins/create_new",array(
+		$data = $adf->post("logins/create_new",array(
 			"login" => $login,
 			"password" => "badTRY"
 		),$options);
-		$this->assertEquals(401,$apf->getStatusCode());
+		$this->assertEquals(401,$adf->getStatusCode());
 		$this->assertNull($data);
 
-		$data = $apf->post("logins/create_new",array(
+		$data = $adf->post("logins/create_new",array(
 			"login" => $login,
 			"password" => "secret1234"
 		),$options);
-		$this->assertEquals(201,$apf->getStatusCode());
+		$this->assertEquals(201,$adf->getStatusCode());
 		$this->assertInternalType("array",$data);
 		$this->assertEquals($login,$data["login"]);
 	}
 
 	function test_lang(){
-		$apf = new ApiDataFetcher("http://skelet.atk14.net/api/",array(
+		$adf = new ApiDataFetcher("http://skelet.atk14.net/api/",array(
 			"logger" => new Logger(),
 			"lang" => "en",
 		));
 
-		$data = $apf->get("login_availabilities/detail",array("login" => "yuri"));
+		$data = $adf->get("login_availabilities/detail",array("login" => "yuri"));
 		$this->assertEquals(array("status" => "available"),$data);
-		$this->assertEquals("http://skelet.atk14.net/api/en/login_availabilities/detail/?login=yuri&format=json",$apf->getLastUrl());
+		$this->assertEquals("http://skelet.atk14.net/api/en/login_availabilities/detail/?login=yuri&format=json",$adf->getLastUrl());
 
-		$data = $apf->get("login_availabilities/detail",array("login" => "yuri", "lang" => "cs"));
+		$data = $adf->get("login_availabilities/detail",array("login" => "yuri", "lang" => "cs"));
 		$this->assertEquals(array("status" => "available"),$data);
-		$this->assertEquals("http://skelet.atk14.net/api/cs/login_availabilities/detail/?login=yuri&format=json",$apf->getLastUrl());
+		$this->assertEquals("http://skelet.atk14.net/api/cs/login_availabilities/detail/?login=yuri&format=json",$adf->getLastUrl());
 
 		// invalid login in the default language (en)
-		$data = $apf->post("logins/create_new",array(
+		$data = $adf->post("logins/create_new",array(
 			"login" => "yuri",
 			"password" => "badass",
 		),array("acceptable_error_codes" => array("404")));
 		$this->assertEquals(null,$data);
-		$this->assertEquals(array("There is no such user"),$apf->getErrors());
+		$this->assertEquals(array("There is no such user"),$adf->getErrors());
 
 		// invalid login in czech
-		$data = $apf->post("logins/create_new",array(
+		$data = $adf->post("logins/create_new",array(
 			"login" => "yuri",
 			"password" => "badass",
 			"lang" => "cs",
 		),array("acceptable_error_codes" => array("404")));
 		$this->assertEquals(null,$data);
-		$this->assertEquals(array("Takový uživatel tady není"),$apf->getErrors());
+		$this->assertEquals(array("Takový uživatel tady není"),$adf->getErrors());
 
 		// suppressing lang
-		$apf = new ApiDataFetcher("http://skelet.atk14.net/api/",array(
+		$adf = new ApiDataFetcher("http://skelet.atk14.net/api/",array(
 			"logger" => new Logger(),
 			"lang" => "",
 		));
 
-		$data = $apf->get("non_existing_resource/detail",array("id" => "123"),array("acceptable_error_codes" => array(404)));
-		$this->assertEquals("http://skelet.atk14.net/api/non_existing_resource/detail/?id=123&format=json",$apf->getLastUrl());
+		$data = $adf->get("non_existing_resource/detail",array("id" => "123"),array("acceptable_error_codes" => array(404)));
+		$this->assertEquals("http://skelet.atk14.net/api/non_existing_resource/detail/?id=123&format=json",$adf->getLastUrl());
 	}
 }
