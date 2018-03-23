1eda6e44d78a98d8222e139df5621b6f4d97ade7
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-03-23 15:47:43 +0100

    ApiDataFetcherPanel tuned

--- src/api_data_fetcher_panel.php
+++ src/api_data_fetcher_panel.php
@@ -1,12 +1,23 @@
 <?php
 class ApiDataFetcherPanel implements Tracy\IBarPanel{
 
-	function __construct($api_data_fetcher){
+	protected $title;
+	protected $api_data_fetcher;
+
+	function __construct($api_data_fetcher, $options = array()){
+		$options += array(
+			"title" => "API",
+		);
+		$this->title = $options["title"];
 		$this->api_data_fetcher = $api_data_fetcher;
 	}
 
 	function getTab(){
-		return "<strong>API</strong> ".$this->api_data_fetcher->getQueriesExecuted();
+		$queries_executed = $this->api_data_fetcher->getQueriesExecuted();
+		if(!$queries_executed){
+			return $this->title;
+		}
+		return "<strong>$this->title</strong> $queries_executed";
 	}
 
 	function getPanel(){
