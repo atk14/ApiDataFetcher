99dcaae9d2d57eed0bde7bbc5c313757d0eb5cda
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-06-20 13:07:24 +0200

    Test fixed

--- test/tc_caching.php
+++ test/tc_caching.php
@@ -2,19 +2,26 @@
 class TcCaching extends TcBase {
 
 	function test(){
-		$apf = new ApiDataFetcher("http://worldclockapi.com/api/json",array("lang" => "")); // non-ATK14 API - so there is need to set empty lang
-		$data1 = $apf->get("/est/now",array(),array("cache" => 60));
+		$apf = new ApiDataFetcher("http://worldtimeapi.org/api/",array("lang" => "")); // non-ATK14 API - so there is need to set empty lang
+		$data1 = $apf->get("timezone/Europe/Prague",array(),array("cache" => 60));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 
 		sleep(2);
 
-		$apf = new ApiDataFetcher("http://worldclockapi.com/api/json",array("lang" => "")); // non-ATK14 API - so there is need to set empty lang
-		$data2 = $apf->get("/est/now",array(),array("cache" => 60));
+		$apf = new ApiDataFetcher("http://worldtimeapi.org/api/",array("lang" => "")); // non-ATK14 API - so there is need to set empty lang
+		$data2 = $apf->get("timezone/Europe/Prague",array(),array("cache" => 60));
+		$this->assertEquals(200,$apf->getStatusCode());
+		$this->assertEquals("OK",$apf->getStatusMessage());
+
+		$data3 = $apf->get("timezone/Europe/Prague",array());
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		
-		$this->assertTrue($data1["currentFileTime"]>0);
-		$this->assertEquals($data1["currentFileTime"],$data2["currentFileTime"]);
+		$this->assertTrue($data1["unixtime"]>0);
+		$this->assertEquals($data1["unixtime"],$data2["unixtime"]);
+
+		$this->assertTrue($data3["unixtime"]>0);
+		$this->assertNotEquals($data1["unixtime"],$data3["unixtime"]);
 	}
 }
