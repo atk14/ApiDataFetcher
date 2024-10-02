40af275656d116a51417fd85b5ff930bdc634bbe
Jaromir Tomek <jaromir.tomek@gmail.com>
2024-10-02 17:24:11 +0200

    [CacheFileStorage] CacheFileStorage::write() can be called with third parameter $expires

--- src/cache_file_storage.php
+++ src/cache_file_storage.php
@@ -11,23 +11,51 @@ class CacheFileStorage{
 		$this->dir = $dir;
 	}
 
-	function read(string $key){
+	/**
+	 *
+	 *	if($cache->readInto("snippet",$content)){
+	 *		echo $content;
+	 *	}
+	 */
+	function readInto(string $key, &$content, &$content_timestamp = null){
+		$content = null;
+		$content_timestamp = null;
+
 		$orig_key = $key;
 		$key = sha1($key);
 		$filename = "$this->dir/$key";
 		if(file_exists($filename)){
-			$content = Files::GetFileContent($filename);
-			$ar = unserialize($content);
-			if($ar && ($ar["key"]==$orig_key)){
-				return $ar["data"];
+			$serialized = Files::GetFileContent($filename);
+			$ar = unserialize($serialized);
+			if($ar && isset($ar["timestamp"]) && $ar["key"]==$orig_key){
+				if(isset($ar["expires"]) && ($ar["timestamp"] + $ar["expires"])<time()){
+					return false;
+				}
+				$content = $ar["data"];
+				$content_timestamp = $ar["timestamp"];
+				return true;
 			}
 		}
+		return false;
 	}
 
-	function write(string $key, mixed $data){
+	function read(string $key){
+		$content = null;
+		$this->readInto($key,$content);
+		return $content;
+	}
+
+	/**
+	 *
+	 *	$cache->write("snippet","<b>content</b>");
+	 *	$cache->write("snippet","<b>content</b>",60); // cache is automatically invalidated after 60 seconds
+	 */
+	function write(string $key, $data, int $expires = null){
 		$content = serialize(array(
 			"key" => $key,
 			"data" => $data,
+			"timestamp" => time(),
+			"expires" => $expires,
 		));
 		$key = sha1($key);
 		Files::Mkdir($this->dir,$err,$err_msg);
