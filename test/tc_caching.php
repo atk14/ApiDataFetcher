69d01151b9d170b8c882fe43105caec79ce484d3
Jaromir Tomek <jaromir.tomek@gmail.com>
2024-10-02 16:11:45 +0200

    Testing uses a different, more reliable external API

--- test/tc_caching.php
+++ test/tc_caching.php
@@ -2,29 +2,29 @@
 class TcCaching extends TcBase {
 
 	function test(){
-		$apf = new ApiDataFetcher("http://worldtimeapi.org/api/",array("lang" => "")); // non-ATK14 API - so there is need to set empty lang
-		$data1 = $apf->get("timezone/Europe/Prague",array(),array("cache" => 60));
+		$apf = new ApiDataFetcher("https://www.timeapi.io/api/",array("lang" => "", "automatically_add_trailing_slash" => false, "default_params" => array())); // non-ATK14 API - so there is need to set empty lang
+		$data1 = $apf->get("timezone/zone",array("timezone" => "Europe/Prague"),array("cache" => 60));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		$raw_response = $apf->getRawResponse();
 
 		sleep(2);
 
-		$apf = new ApiDataFetcher("http://worldtimeapi.org/api/",array("lang" => "")); // non-ATK14 API - so there is need to set empty lang
-		$data2 = $apf->get("timezone/Europe/Prague",array(),array("cache" => 60));
+		$apf = new ApiDataFetcher("https://www.timeapi.io/api/",array("lang" => "", "automatically_add_trailing_slash" => false, "default_params" => array())); // non-ATK14 API - so there is need to set empty lang
+		$data2 = $apf->get("timezone/zone",array("timezone" => "Europe/Prague"),array("cache" => 60));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		$this->assertEquals($raw_response,$apf->getRawResponse());
 
-		$data3 = $apf->get("timezone/Europe/Prague",array());
+		$data3 = $apf->get("timezone/zone",array("timezone" => "Europe/Prague"));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		$this->assertNotEquals($raw_response,$apf->getRawResponse());
 		
-		$this->assertTrue($data1["unixtime"]>0);
-		$this->assertEquals($data1["unixtime"],$data2["unixtime"]);
+		$this->assertTrue(strtotime($data1["currentLocalTime"])>0);
+		$this->assertEquals(strtotime($data1["currentLocalTime"]),strtotime($data2["currentLocalTime"]));
 
-		$this->assertTrue($data3["unixtime"]>0);
-		$this->assertNotEquals($data1["unixtime"],$data3["unixtime"]);
+		$this->assertTrue(strtotime($data3["currentLocalTime"])>0);
+		$this->assertNotEquals(strtotime($data1["currentLocalTime"]),strtotime($data3["currentLocalTime"]));
 	}
 }
