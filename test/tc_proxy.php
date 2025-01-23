dba22bf7bb1e6bb7b765cc376df68dfaed60d3e6
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-01-23 14:56:19 +0100

    Testing doesn't fail when privoxy is not running

--- test/tc_proxy.php
+++ test/tc_proxy.php
@@ -2,6 +2,12 @@
 class TcProxy extends TcBase {
 
 	function test(){
+		if(!$this->_is_privoxy_running()){
+			file_put_contents("php://stderr","WARNING: privoxy is not running; skip testing");
+			$this->assertEquals(1,1);
+			return;
+		}
+
 		$adf = new ApiDataFetcher("https://www.atk14.net/api/",array(
 			"logger" => new Logger(),
 			"proxy" => "tcp://127.0.0.1:8118",
@@ -20,5 +26,13 @@ class TcProxy extends TcBase {
 			$exception_message = $e->getMessage();
 		}
 		$this->assertStringContains("could not connect to proxy server tcp://127.0.0.1:8119",$exception_message);
+	}
+
+	function _is_privoxy_running(){
+		$uf = new UrlFetcher("http://127.0.0.1:8118/");
+		if(@is_null($uf->getStatusCode())){
+			return false;
+		}
+		return true;
 	}
 }
