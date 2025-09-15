49661322c50ab834a458776e7b7564617b87b598
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-09-15 14:47:54 +0200

    Tests fixed

--- test/tc_caching.php
+++ test/tc_caching.php
@@ -4,17 +4,18 @@ class TcCaching extends TcBase {
 	function test(){
 		$cache_storage = new CacheFileStorage(Files::GetTempDir()."/".uniqid()."/");
 
+		$api_url = "https://www.atk14.net/api/";
+		$endpoint = "timezones/detail";
+		$endpoint_params = array("timezone" => "Europe/Prague");
+
 		$apf = new ApiDataFetcher(
-			"https://www.timeapi.io/api/",
+			$api_url,
 			array(
 				"cache_storage" => $cache_storage,
-				"lang" => "",  // non-ATK14 API - so there is need to set empty lang
-				"automatically_add_trailing_slash" => false,
-				"default_params" => array(),
 				"socket_timeout" => 10.0,
 			)
 		);
-		$data1 = $apf->get("timezone/zone",array("timezone" => "Europe/Prague"),array("cache" => 60));
+		$data1 = $apf->get($endpoint,$endpoint_params,array("cache" => 60));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		$this->assertEquals(false,$apf->isResponseCached());
@@ -23,31 +24,28 @@ class TcCaching extends TcBase {
 		sleep(2);
 
 		$apf = new ApiDataFetcher(
-			"https://www.timeapi.io/api/",
+			$api_url,
 			array(
 				"cache_storage" => $cache_storage,
-				"lang" => "",
-				"automatically_add_trailing_slash" => false,
-				"default_params" => array(),
 				"socket_timeout" => 10.0
 			)
 		);
-		$data2 = $apf->get("timezone/zone",array("timezone" => "Europe/Prague"),array("cache" => 60));
+		$data2 = $apf->get($endpoint,$endpoint_params,array("cache" => 60));
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		$this->assertEquals(true,$apf->isResponseCached());
 		$this->assertEquals($raw_response,$apf->getRawResponse());
 
-		$data3 = $apf->get("timezone/zone",array("timezone" => "Europe/Prague"));
+		$data3 = $apf->get($endpoint,$endpoint_params);
 		$this->assertEquals(200,$apf->getStatusCode());
 		$this->assertEquals("OK",$apf->getStatusMessage());
 		$this->assertEquals(false,$apf->isResponseCached());
 		$this->assertNotEquals($raw_response,$apf->getRawResponse());
 		
-		$this->assertTrue(strtotime($data1["currentLocalTime"])>0);
-		$this->assertEquals(strtotime($data1["currentLocalTime"]),strtotime($data2["currentLocalTime"]));
+		$this->assertTrue($data1["datetime"]>0);
+		$this->assertEquals($data1["datetime"],$data2["datetime"]);
 
-		$this->assertTrue(strtotime($data3["currentLocalTime"])>0);
-		$this->assertNotEquals(strtotime($data1["currentLocalTime"]),strtotime($data3["currentLocalTime"]));
+		$this->assertTrue(strtotime($data3["datetime"])>0);
+		$this->assertNotEquals(strtotime($data1["datetime"]),strtotime($data3["datetime"]));
 	}
 }
