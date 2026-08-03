32c1e7be4cb92bef337d3cbb8ad8155a85c1e9d1
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-08-03 12:07:56 +0200

    CacheFileStorage moved into package atk14/cache-file-storage

--- test/initialize.php
+++ test/initialize.php
@@ -2,7 +2,6 @@
 define("TEST",true);
 
 require(__DIR__ . "/../src/api_data_fetcher.php");
-require(__DIR__ . "/../src/cache_file_storage.php");
 require(__DIR__ . "/testing_logger.php");
 
 require(__DIR__ . "/article.php");
